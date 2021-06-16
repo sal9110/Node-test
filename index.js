84fcc7d06548f89411b865d24d0173372c3c6e3f
Salvo <salvopatti110@gmail.com>
2021-06-16 17:04:17 +0200

    uploading file

---
+++ index.js
@@ -0,0 +1,8 @@
+const chalk = require('chalk');
+
+console.log(chalk.greenBright('Pilar'))
+console.log(chalk.cyanBright('Lorianne'))
+console.log(chalk.bgWhiteBright('Victor'))
+console.log(chalk.bgBlue('G'))
+
+
